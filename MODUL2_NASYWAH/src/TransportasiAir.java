d3b4cebceec4a5a64ee921d4f3fb3ab047d99faf
nasywahhhhh <naswahputri88c@gmail.com>
2022-12-10 07:52:38 +0700

    MODUL2_NASYWAH

---
+++ MODUL2_NASYWAH/src/TransportasiAir.java
@@ -0,0 +1,20 @@
+public class TransportasiAir {
+    protected int jumlahKursi;
+    protected int biaya;
+
+    public TransportasiAir(int jumlahKursi, int biaya){
+        this.jumlahKursi= jumlahKursi;
+        this.biaya= biaya;
+    }
+    public void informasi(){
+        System.out.println("Transportasi Air jenis tidak diketahui dengan kursi berjumlah "+jumlahKursi+" dengan biaya sebesar Rp. "+biaya+"");
+    
+    }
+    public void berlayar(){
+        System.out.println("Transportasi Air dengan jenis tidak diketahui sedang berlayar");
+
+    }
+    public void berlabuh(){
+        System.out.println("Transportasi Air dengan jenis tidak diketahui berlabuh di pantai");
+    }
+}
